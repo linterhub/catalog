e1a9d2f01c2725a393b471f2954c944e64a3d935
Oleg Safonov <by.xsofa@gmail.com>
2018-05-19 16:56:48 +0300

    copy cname file to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,19 @@ var path = {
     dest: './build'
 };
 
+gulp.task('static', function() {
+    var stream = gulp
+        .src('CNAME')
+        .pipe(gulp.dest('build'));
+    return stream;
+})
+
+
 // Clean output
 gulp.task('clean', function () {
     return del([
         path.dest + '/**/*',
     ]);
 });
+
+gulp.task('default', ['static']);
